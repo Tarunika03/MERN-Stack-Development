f724eb8c3b55ad7e396d65733b9e636ecb3edff6
Tarunika03 <tarunika.20jics066@jietjodhpur.ac.in>
2022-12-09 13:22:05 +0530

    commit

--- project_01/servers/db/conn.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const DB = process.env.DATABASE;
-mongoose.connect(DB, (err) => {
-        if (!err) console.log('db connected');
-        else console.log(err)
-    }
-
-)
